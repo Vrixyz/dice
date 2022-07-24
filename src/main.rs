5eea815d5d9f0ab70dbab101b8eeb9943b648240
Thierry Berger <contact@thierryberger.com>
2022-07-24 14:35:45 +0200

    space to spawn cubes

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use bevy::prelude::*;
 use bevy_rapier3d::prelude::*;
 
+pub struct DiceMesh(Handle<Mesh>);
+pub struct DiceMaterial(Handle<StandardMaterial>);
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(
@@ -11,9 +14,10 @@ fn main() {
         .insert_resource(Msaa::default())
         .add_plugins(DefaultPlugins)
         .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
-        .add_plugin(RapierDebugRenderPlugin::default())
+        //.add_plugin(RapierDebugRenderPlugin::default())
         .add_startup_system(setup_graphics)
         .add_startup_system(setup_physics)
+        .add_system(spawn_new_dice)
         .run();
 }
 
@@ -31,7 +35,11 @@ fn setup_graphics(mut commands: Commands) {
     });
 }
 
-pub fn setup_physics(mut commands: Commands) {
+pub fn setup_physics(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
     /*
      * Ground
      */
@@ -46,47 +54,75 @@ pub fn setup_physics(mut commands: Commands) {
         )))
         .insert(Collider::cuboid(ground_size, ground_height, ground_size));
 
-    /*
-     * Create the cubes
-     */
-    let num = 2;
-    let rad = 1.0;
+    // Light
 
-    let shift = rad * 2.0 + rad;
-    let centerx = shift * (num / 2) as f32;
-    let centery = shift / 2.0;
-    let centerz = shift * (num / 2) as f32;
+    commands.insert_resource(AmbientLight {
+        color: Color::WHITE,
+        brightness: 0.02,
+    });
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 9000.0,
+            shadows_enabled: true,
+            range: 100.0,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 10.0),
+        ..default()
+    });
 
-    let mut offset = -(num as f32) * (rad * 2.0 + rad) * 0.5;
-    let mut color = 0;
-    let colors = [
-        Color::hsl(220.0, 1.0, 0.3),
-        Color::hsl(180.0, 1.0, 0.3),
-        Color::hsl(260.0, 1.0, 0.7),
-    ];
+    // load meshes
+    commands.insert_resource(DiceMesh(meshes.add(Mesh::from(shape::Cube { size: 2.0 }))));
+    commands.insert_resource(DiceMaterial(
+        materials.add(Color::rgb(0.2, 1.0, 0.2).into()),
+    ));
+}
 
-    for j in 0usize..8 {
-        for i in 0..num {
-            for k in 0usize..num {
-                let x = i as f32 * shift - centerx + offset;
-                let y = j as f32 * shift + centery + 3.0;
-                let z = k as f32 * shift - centerz + offset;
-                color += 1;
+fn spawn_new_dice(
+    mut commands: Commands,
+    mut dice_mesh: ResMut<DiceMesh>,
+    mut dice_material: ResMut<DiceMaterial>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    keys: Res<Input<KeyCode>>,
+) {
+    if keys.just_pressed(KeyCode::Space) {
+        /*
+         * Create the cubes
+         */
+        let num = 2;
+        let rad = 1.0;
 
-                commands
-                    .spawn_bundle(TransformBundle::from(Transform::from_rotation(
-                        Quat::from_rotation_x(0.2),
-                    )))
-                    .with_children(|child| {
-                        child
-                            .spawn_bundle(TransformBundle::from(Transform::from_xyz(x, y, z)))
-                            .insert(RigidBody::Dynamic)
-                            .insert(Collider::cuboid(rad, rad, rad))
-                            .insert(ColliderDebugColor(colors[color % 3]));
-                    });
-            }
-        }
+        let centerx = (num / 2) as f32;
+        let centery = 0f32;
+        let centerz = (num / 2) as f32;
 
-        offset -= 0.05 * rad * (num as f32 - 1.0);
+        let mut color = 0;
+        let colors = [
+            Color::hsl(220.0, 1.0, 0.3),
+            Color::hsl(180.0, 1.0, 0.3),
+            Color::hsl(260.0, 1.0, 0.7),
+        ];
+
+        let x = -centerx;
+        let y = centery + 3.0;
+        let z = centerz;
+        color += 1;
+
+        commands
+            .spawn_bundle(TransformBundle::from(Transform::from_rotation(
+                Quat::from_rotation_x(0.2),
+            )))
+            .with_children(|child| {
+                child
+                    .spawn_bundle(PbrBundle {
+                        mesh: dice_mesh.0.clone(),
+                        material: dice_material.0.clone(),
+                        ..default()
+                    })
+                    .insert_bundle(TransformBundle::from(Transform::from_xyz(x, y, z)))
+                    .insert(RigidBody::Dynamic)
+                    .insert(Collider::cuboid(rad, rad, rad))
+                    .insert(ColliderDebugColor(colors[color % 3]));
+            });
     }
 }
